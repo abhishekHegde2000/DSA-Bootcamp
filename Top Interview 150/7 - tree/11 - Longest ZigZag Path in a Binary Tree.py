61830eb9f0e04156395eafd472499a0edab4cc4e
Abhishek Hegde <ahegde2021@gmail.com>
2024-01-19 17:00:03 +0530

    commit

--- Top Interview 150/7 - tree/11 - Longest ZigZag Path in a Binary Tree.py
+++ Top Interview 150/7 - tree/11 - Longest ZigZag Path in a Binary Tree.py
@@ -53,14 +53,22 @@ class Solution:
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         self.maxLength = 0
 
-        def solve(node, deep, dir):
-            self.maxLength = max(self.maxLength, deep)
+        def solve(node, depth, direction):
+            print(f"Visiting node: {node.val}, depth: {
+                  depth}, direction: {direction}")
+            self.maxLength = max(self.maxLength, depth)
+            print(f"Updated maxLength: {self.maxLength}")
 
             if node.left is not None:
-                solve(node.left, deep+1,
-                      'left') if dir != 'left' else solve(node.left, 1, 'left')
+                print(f"Going {
+                      'same' if direction == 'left' else 'opposite'} direction to left child of node: {node.val}")
+                solve(node.left, depth+1,
+                      'left') if direction != 'left' else solve(node.left, 1, 'left')
             if node.right is not None:
-                solve(node.right, deep+1,
-                      'right') if dir != 'right' else solve(node.right, 1, 'right')
+                print(f"Going {
+                      'same' if direction == 'right' else 'opposite'} direction to right child of node: {node.val}")
+                solve(node.right, depth+1,
+                      'right') if direction != 'right' else solve(node.right, 1, 'right')
+
         solve(root, 0, '')
         return self.maxLength
