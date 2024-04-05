968e0f4b15dd6861ab8981dbe77d66eb37b4d0a5
Abhishek Hegde <ahegde2021@gmail.com>
2024-04-05 10:06:12 +0530

    commit

--- Top 100 Liked/8 - backtracking/1 - Letter Combinations of a Phone Number.py
+++ Top 100 Liked/8 - backtracking/1 - Letter Combinations of a Phone Number.py
@@ -3,9 +3,11 @@ https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
 
 17. Letter Combinations of a Phone Number
 
-Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+Given a string containing digits from 2-9 inclusive, return all possible letter 
+combinations that the number could represent. Return the answer in any order.
 
-A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+A mapping of digits to letters (just like on the telephone buttons) is given below. 
+Note that 1 does not map to any letters.
 
 
  
@@ -14,6 +16,7 @@ Example 1:
 
 Input: digits = "23"
 Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+
 Example 2:
 
 Input: digits = ""
--- Top 100 Liked/8 - backtracking/2 - Generate Parentheses.py
+++ Top 100 Liked/8 - backtracking/2 - Generate Parentheses.py
@@ -3,7 +3,9 @@ https://leetcode.com/problems/generate-parentheses/?envType=study-plan-v2&envId=
 
 22. Generate Parentheses
 
-Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+Given n pairs of parentheses, 
+write a function to generate all combinations of 
+well-formed parentheses.
 
  
 
