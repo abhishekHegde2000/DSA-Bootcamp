cc183efbb9583b8abc976b95c372cdc5184d2cc9
Abhishek Hegde <ahegde2021@gmail.com>
2025-08-04 20:27:43 +0530

    commit

---
+++ TUF/Step 1 - Learn the basics/Lec 1 - Things to Know in C++ Java Python or any language/1 - Input Output.py
@@ -0,0 +1,21 @@
+# Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+
+
+# Use:-
+
+# for C++ : cout << variable_name;
+# for Java : System.out.print();
+# for Python : print()
+# for Javascript : console.log()
+
+class Solution:
+    def printNumber(self):
+
+        number = int(input(" Enter a number: "))
+        # Print the number using the appropriate method for the language
+        print(number)
+
+
+sol = Solution()
+sol.printNumber()
---
+++ TUF/Step 1 - Learn the basics/Lec 1 - Things to Know in C++ Java Python or any language/2 - If Else.py
@@ -0,0 +1,50 @@
+'''
+
+If ElseIf
+100
+Hint
+Given marks of a student, print on the screen:
+
+Grade A if marks >= 90
+Grade B if marks >= 70
+Grade C if marks >= 50
+Grade D if marks >= 35
+Fail, otherwise.
+
+
+For printing use:-
+
+for C++ : cout << variable_name;
+for Java : System.out.print();
+for Python : print()
+for Javascript : console.log()
+
+
+
+Examples:
+Input: marks = 95
+
+Output: Grade A
+
+Explanation: marks are greater than or equal to 90.
+
+Input: marks = 14
+
+Output: Fail
+
+Explanation: marks are less than 35.
+'''
+
+
+class Solution:
+    def studentGrade(self, marks):
+        if marks >= 90:
+            print("Grade A")
+        elif marks >= 70:
+            print("Grade B")
+        elif marks >= 50:
+            print("Grade C")
+        elif marks >= 35:
+            print("Grade D")
+        else:
+            print("Fail")
